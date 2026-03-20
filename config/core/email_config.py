32bf6f980971843a53fd44e6a9df7d387f7c5553
Opeyemi Akande <akande212@gmail.com>
2026-03-20 11:19:36 +0100

    updated email service config

--- config/core/email_config.py
+++ config/core/email_config.py
@@ -1,14 +1,22 @@
 # app/core/config.py
 from pydantic_settings import BaseSettings
+import os
+
+smtp_user = os.getenv("SMTP_USER")
+smtp_password = os.getenv("SMTP_PASSWORD")
+email_from = os.getenv("EMAIL_FROM")
+frontend_url = os.getenv("FRONTEND_URL")
+smtp_host = os.getenv("SMTP_HOST")
+smtp_port = os.getenv("SMTP_PORT")
 
 
 class Settings(BaseSettings):
-    SMTP_HOST: str = "smtp.gmail.com"
-    SMTP_PORT: int = 587
-    SMTP_USER: str = "akande212@gmail.com"
-    SMTP_PASSWORD: str = "qkub camd fuao rjrm"
-    EMAIL_FROM: str = "akande212@gmail.com"
-    FRONTEND_URL: str = "http://localhost:3000"
+    SMTP_HOST: str = smtp_host  # type: ignore
+    SMTP_PORT: int = smtp_port  # type: ignore
+    SMTP_USER: str = smtp_user  # type: ignore
+    SMTP_PASSWORD: str = smtp_password  # type: ignore
+    EMAIL_FROM: str = email_from  # type: ignore
+    FRONTEND_URL: str = frontend_url  # type: ignore
 
 
 settings = Settings()
